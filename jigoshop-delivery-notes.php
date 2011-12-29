737e7fd79bd88b4e926bc18398d3ec5e2f895ea5
Triggvy Gunderson <wartamau@gmail.com>
2011-12-29 14:51:12 +0100

    added license note

--- jigoshop-delivery-notes.php
+++ jigoshop-delivery-notes.php
@@ -1,12 +1,18 @@
 <?php
 /*
-   Plugin Name: JigoShop - Delivery Notes
-   Plugin URI: https://github.com/piffpaffpuff/jigoshop-delivery-notes
-   Description: Basic Jigoshop Delivery Notes
-   Version: 1.1
-   Author: Steve Clark, Trigvvy Gunderson
-   Author URI: https://github.com/piffpaffpuff/jigoshop-delivery-notes
- */
+	Plugin Name: JigoShop - Delivery Notes
+	Plugin URI: https://github.com/piffpaffpuff/jigoshop-delivery-notes
+	Description: Basic Jigoshop Delivery Notes
+	Version: 1.1
+	Author: Steve Clark, Trigvvy Gunderson
+	Author URI: https://github.com/piffpaffpuff/jigoshop-delivery-notes
+	License: GPL3
+	
+	This plugin is distributed under the 
+	GNU General Public License; Version 3 (v3), 
+	and is thus certified open source software. 
+	See http://www.gnu.org/licenses/gpl.html.
+*/
 
 require_once 'jigoshop-delivery-notes-classes.php';
 
